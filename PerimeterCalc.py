61b2ba5f389ede1592e9b8a56d059e586a725ffe
everyones-a-critic <ealy.matthew.d@gmail.com>
2017-01-30 20:44:23 -0500

    Cleaned up assignment code

--- PerimeterCalc.py
+++ PerimeterCalc.py
@@ -1,24 +1,22 @@
 # Collect input on paper dimensions
 # Validating input to ensure our input can be multiplied
-
-
-def get_number(message):
+# defining a generic function allows for consistent input of a given type
+def get_number(prompt):
     while True:
         try:
-            n = int(input(message))
+            n = int(input(prompt))
         except ValueError:
             print("Invalid entry. Please enter an integer value.")
             continue
         else:
             return n
-            break
 
 
 # Assign l and w by calling functions
 l = get_number("Please enter the length of the paper in millimeters:")
 w = get_number("Please enter the width of the paper in millimeters:")
 # P = 2(l + w).
-perimeter = 2 * ( l + w )
+perimeter = 2 * (l + w)
 # Concatenate the string with the output from our perimeter calculation
 # convert int to str in order to join them in one line
 print("The perimeter of the page is " + str(perimeter) + "mm")
